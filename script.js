cece91a58c2d06ff03b2a4279e9a7f42a6949543
UniverseNature <70004605+UniverseNature@users.noreply.github.com>
2020-12-12 13:16:35 +0530

    read the desc
    added comments, finished the sketch settings, the theme changer works properly

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let message = "If it is possible, keep your browser's zoom level at 100%. It will help the website look better.";
 //alert(message);
 
+// A function, called when we want to change the theme to DARK.
+
 function themeChangerDark(){
    document.body.style.backgroundColor = "black";
    document.body.style.color = "white";
    document.getElementById("themeChangerDark").style.display = "none";
    document.getElementById("themeChangerLight").style.display = "inline";
+   document.getElementById("sketchSettings").style.border = "2px solid white";
+   document.getElementById("pxAmount").style.backgroundColor = "white";
+   document.getElementById("inputCustomValue").style.backgroundColor = "white";
+   document.getElementById("inputCustomValue").style.border = "2px solid black";
 }
+
+// A function, called when we want to change the theme to LIGHT.
 
 function themeChangerLight(){
     document.body.style.backgroundColor = "lightyellow";
     document.body.style.color = "black";
     document.getElementById("themeChangerLight").style.display = "none";
     document.getElementById("themeChangerDark").style.display = "inline";
+    document.getElementById("sketchSettings").style.border = "2px solid black";
+    document.getElementById("pxAmount").style.backgroundColor = "lightyellow";
+    document.getElementById("inputCustomValue").style.backgroundColor = "lightyellow";
+    document.getElementById("inputCustomValue").style.border = "2px solid black";
 }
