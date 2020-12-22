cef7efcd55e65ec1b90042245185aa4b9717f37a
UniverseNature <70004605+UniverseNature@users.noreply.github.com>
2020-12-22 14:24:17 +0530

    read the description of this commit
    made the divmaker, made the themechanger more efficient, layout now uses flex, made it so that it displays the text field if the dropdown value is custom, canvas clearer is not yet complete

--- script.js
+++ script.js
@@ -1,28 +1,86 @@
 let message = "If it is possible, keep your browser's zoom level at 100%. It will help the website look better.";
 //alert(message);
 
+// Declare all the variables we need
+
+let darkBtn = document.querySelector("#themeChangerDark");
+let lightBtn = document.querySelector("#themeChangerLight");
+let pxBtn = document.querySelector("#submitCustomValue");
+let pxAmt;
+let selectElement =  document.querySelector("#pxAmount"); 
+let output; 
+let clear = document.querySelector("#clearPxAmount");
+
 // A function, called when we want to change the theme to DARK.
 
-function themeChangerDark(){
+function themeChangerDark() {
    document.body.style.backgroundColor = "black";
    document.body.style.color = "white";
    document.getElementById("themeChangerDark").style.display = "none";
    document.getElementById("themeChangerLight").style.display = "inline";
-   document.getElementById("sketchSettings").style.border = "2px solid white";
-   document.getElementById("pxAmount").style.backgroundColor = "white";
-   document.getElementById("inputCustomValue").style.backgroundColor = "white";
-   document.getElementById("inputCustomValue").style.border = "2px solid black";
+   document.getElementById("sketchSettings").style.border = "1px solid white";
 }
 
 // A function, called when we want to change the theme to LIGHT.
 
-function themeChangerLight(){
+function themeChangerLight() {
     document.body.style.backgroundColor = "lightyellow";
     document.body.style.color = "black";
     document.getElementById("themeChangerLight").style.display = "none";
     document.getElementById("themeChangerDark").style.display = "inline";
-    document.getElementById("sketchSettings").style.border = "2px solid black";
-    document.getElementById("pxAmount").style.backgroundColor = "lightyellow";
-    document.getElementById("inputCustomValue").style.backgroundColor = "lightyellow";
-    document.getElementById("inputCustomValue").style.border = "2px solid black";
+    document.getElementById("sketchSettings").style.border = "1px solid black";
 }
+
+// A function, called when the user submits the amount of pixels.
+
+function divMaker() {
+    for (var i = 0; i < output; i++) {
+        let container = document.querySelector("#container");
+        let div = document.createElement("div");
+        div.classList = "containerChildren";
+        div.backgroundColor = "blue";
+        div.textContent = "Hello World!";
+        container.appendChild(div);
+    }
+}
+
+// A function, called when the user presses the button to clear the canvas.
+
+function clearPxAmount(){
+    let container = document.querySelector("#container");
+    for (var i = 0; i < output; i++) {
+        container.removeChild("containerChildren"); 
+    }
+}
+
+// Event listeners for changing the theme.
+
+darkBtn.addEventListener('click', () => {
+    themeChangerDark()
+});
+
+lightBtn.addEventListener('click', () => {
+    themeChangerLight()
+});
+
+clear.addEventListener('click', () => {
+    clearPxAmount();
+});
+
+/* If the pixel-amount submit button is clicked, asign the value to a variable and if the value is "custom",
+   change the buttons position and make the custom value bar appear. */
+
+pxBtn.addEventListener('click', () => {
+    output  = selectElement.value;
+    divMaker()
+
+    if (output === "custom") {
+        document.getElementById("inputCustomValue").style.display = "inline";
+        document.getElementById("submitCustomValue").style.float = "right";
+        document.getElementById("submitCustomValue").style.marginRight = "2em";
+    } else {
+        document.getElementById("inputCustomValue").style.display = "none";
+        document.getElementById("submitCustomValue").style.float = "none";
+        document.getElementById("submitCustomValue").style.marginRight = "none";
+    }
+});
